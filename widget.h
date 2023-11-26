45a3a5657b4586283492d48bdd5124446cc9e5d4
soldiervinz <vinzenz.uhr@gmail.com>
2023-11-26 12:26:18 +0100

    setup QOpenGLWidget

--- widget.h
+++ widget.h
@@ -1,13 +1,13 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
-#include <QWidget>
+#include <QOpenGLWidget>
 
 namespace Ui {
 class widget;
 }
 
-class widget : public QWidget
+class widget : public QOpenGLWidget
 {
     Q_OBJECT
 
@@ -15,8 +15,13 @@ public:
     explicit widget(QWidget *parent = nullptr);
     ~widget();
 
+private slots:
+    void DisplayImage();
+
 private:
     Ui::widget *ui;
+    QImage imdisplay;  //This will create QImage which is shown in Qt label
+    QTimer* Timer;   // A timer is needed in GUI application
 };
 
 #endif // WIDGET_H
